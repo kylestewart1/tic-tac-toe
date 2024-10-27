2ae325f0ee02f8b5de79ddda634c9afefacb4aa7
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-10-27 13:29:36 -0400

    implemented win/tie conditions, cell buttons now deactivate when game over

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -70,12 +70,61 @@ function GameController(xPlayerName, oPlayerName) {
         activePlayer = (activePlayer === players.X) ? players.O : players.X;
     }
 
+    const calcScores = () => {
+        const scoreBoard = board.getBoard().map(row => row.map(cell => {
+            const val = cell.getValue();
+            if (val == players.X.getSymbol()) {
+                return 1;
+            } else if (val == players.O.getSymbol()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+            ));
+        const colSums = [];
+        const rowSums = [];
+        const diagSums = [scoreBoard[0][0]+scoreBoard[1][1]+scoreBoard[2][2],
+                          scoreBoard[2][0]+scoreBoard[1][1]+scoreBoard[0][2]];
+        for (let i=0; i<3; i++) {
+            let col = 0;
+            let row = 0;
+            for (let j=0; j<3; j++) {
+                col += scoreBoard[j][i];
+                row += scoreBoard[i][j];
+            }
+            colSums.push(col);
+            rowSums.push(row);
+        }
+        return {
+            row: rowSums,
+            column: colSums,
+            diagonal: diagSums
+        };
+    }
+
+    const checkWinner = (scores) => {
+        for (const key in scores) {
+            if (Math.max(...scores[key]) == 3) {
+                return players.X;
+            } else if (Math.min(...scores[key]) == -3) {
+                return players.O;
+            } 
+        }
+        return null;
+    }
+
     const playRound = (row, column) => {
         const validMove = (board.getBoard()[row][column].getValue() === null);
         if (!validMove) {
             return;
         }
         board.fill(row, column, getActivePlayer().getSymbol());
+        const scores = calcScores();
+        const winner = checkWinner(scores);
+        if (winner) {
+            return true;
+        }
         switchPlayerTurn();
         printRoundInfo();
     }
@@ -91,6 +140,7 @@ function ScreenController() {
     const body = document.querySelector("body");
     const game = GameController("Xzibit", "Odawg");
     const boardDiv = document.querySelector(".game-board");
+    let gameOver = false;
 
     const update = () => {
         body.classList = [game.getActivePlayer().getSymbol()];
@@ -101,7 +151,9 @@ function ScreenController() {
             row.forEach((cell, column) => {
                 const cellButton = document.createElement("button");
                 cellButton.classList.add("cell");
-
+                if (!gameOver) {
+                    cellButton.classList.add("active");
+                }
                 cellButton.dataset.row = rowNum;
                 cellButton.dataset.column = column; 
                 cellButton.textContent = cell.getValue();
@@ -111,13 +163,16 @@ function ScreenController() {
     }
 
     const clickHandlerBoard = (e) => {
-        const row = e.target.dataset.row;
-        const column = e.target.dataset.column;
-        if (!row || !column) {
-            return;
-        }
-        game.playRound(row, column);
-        update();
+        if (!gameOver) {
+            const row = e.target.dataset.row;
+            const column = e.target.dataset.column;
+            if (!row || !column) {
+                return;
+            }
+            gameOver = game.playRound(row, column);
+            
+            update();
+        } 
     }
 
     boardDiv.addEventListener("click", clickHandlerBoard);
